147815260c327aac5a904a765caecabeb4d98afc
MATIJA SIMIN <marcussimon12082015@hotmail.com>
2017-09-13 21:14:51 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ function takeANumber(katzDeli, ime){
 
 function nowServing(array){
   if (array.length === 0) {
-    return "The line is currently empty.";
+    return "There is nobody waiting to be served!";
   }
+
 }
