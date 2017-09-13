447b79e541cd2f82b4c8488eeba9d0776e04c669
MATIJA SIMIN <marcussimon12082015@hotmail.com>
2017-09-13 21:13:58 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,3 +4,9 @@ function takeANumber(katzDeli, ime){
   katzDeli.push(ime);
   return `Welcome, ${ime}. You are number ${katzDeli.length} in line.`;
 }
+
+function nowServing(array){
+  if (array.length === 0) {
+    return "The line is currently empty.";
+  }
+}
